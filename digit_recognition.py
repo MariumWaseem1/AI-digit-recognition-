933331bd220eeecb3edbbc52172f7a45a428e6d4
marium waseem <132593290+MariumWaseem1@users.noreply.github.com>
2024-06-28 02:36:20 +0500

    Update digit_recognition.py

--- digit_recognition.py
+++ digit_recognition.py
@@ -1,37 +1,4 @@
-# import tensorflow as tf
-# from tensorflow.keras import layers, models
-# from tensorflow.keras.datasets import mnist
 
-
-# # Load MNIST dataset
-# mnist = tf.keras.datasets.mnist
-# (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
-
-
-
-# # Normalize pixel values to be between 0 and 1
-# train_images, test_images = train_images / 255.0, test_images / 255.0
-
-# model = models.Sequential([
-#     layers.Flatten(input_shape=(28, 28)),  # Flatten the 28x28 images into a 1D array
-#     layers.Dense(128, activation='relu'),  # Fully connected layer with 128 units and ReLU activation
-#     layers.Dropout(0.2),  # Dropout layer to reduce overfitting
-#     layers.Dense(10, activation='softmax')  # Output layer with 10 units for 0-9 digits
-# ])
-
-# model.compile(optimizer='adam',
-#               loss='sparse_categorical_crossentropy',
-#               metrics=['accuracy'])
-
-# model.fit(train_images, train_labels, epochs=5)
-
-# test_loss, test_acc = model.evaluate(test_images, test_labels)
-# print(f'Test accuracy: {test_acc}')
-
-# predictions = model.predict(test_images)
-
-# model.save('digit_recognition_model.h5')
-# loaded_model = models.load_model('digit_recognition_model.h5')
 
 
 import tensorflow as tf
